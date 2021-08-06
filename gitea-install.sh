37dbdd2996f0ddffad81cfad4e44613a7ed8fdc2
npope <npope@protonmail.com>
2021-08-05 19:49:55 -0500

    Fixing sha256 checksum compare so it will follow links if needed to get file, also updating so installer only installs Current Release and not RC

--- gitea-install.sh
+++ gitea-install.sh
@@ -116,12 +116,12 @@ fi
 
 # Download latest version of Gitea and install
 # https://docs.gitea.io/en-us/install-from-binary/
-GITEA_LATEST_VERSION=$(curl -s https://dl.gitea.io/gitea/ | grep '<a href\=\"\/gitea' | cut -d"/" -f3 | cut -d '"' -f1 | grep '[[:digit:]]' | sort -V | tail -n 1)
+GITEA_LATEST_VERSION=$(curl -L -s https://dl.gitea.io/gitea/ | grep '<a href\=\"\/gitea' | cut -d"/" -f3 | grep 'Current Release' | cut -d '"' -f1 | grep '[[:digit:]]' | sort -V | tail -n 1)
 cd /tmp >> /dev/null 2>&1
 rm gitea gitea.sha256 >> /dev/null 2>&1
 # download signature
 debugecho 'downloading gitea.sha256 signature'
-curl --output gitea.sha256 https://dl.gitea.io/gitea/${GITEA_LATEST_VERSION}/gitea-${GITEA_LATEST_VERSION}-linux-amd64.sha256 >> /dev/null 2>&1
+curl -L --output gitea.sha256 https://dl.gitea.io/gitea/${GITEA_LATEST_VERSION}/gitea-${GITEA_LATEST_VERSION}-linux-amd64.sha256 >> /dev/null 2>&1
 GITEA_LATEST_VERSION_SHA256SUM=$(cut -d' ' -f1 gitea.sha256 | tr -d '\040\011\012\015')
 debugecho "GITEA_LATEST_VERSION_SHA256SUM=${GITEA_LATEST_VERSION_SHA256SUM}"
 # check to see if we need to update
@@ -141,7 +141,7 @@ if [[ "${GITEA_CURRENT_VERSION_BIN_SHA256SUM}" == "${GITEA_LATEST_VERSION_SHA256
 else
   debugecho 'downloading gitea binary, this will take a little bit'
   # download binary
-  curl --output gitea https://dl.gitea.io/gitea/${GITEA_LATEST_VERSION}/gitea-${GITEA_LATEST_VERSION}-linux-amd64 >> /dev/null 2>&1
+  curl -L --output gitea https://dl.gitea.io/gitea/${GITEA_LATEST_VERSION}/gitea-${GITEA_LATEST_VERSION}-linux-amd64 >> /dev/null 2>&1
   # validate download
   GITEA_LATEST_VERSION_BIN_SHA256SUM=$(sha256sum gitea | cut -d' ' -f1 | tr -d '\040\011\012\015')
   if [[ "${GITEA_LATEST_VERSION_BIN_SHA256SUM}" == "${GITEA_LATEST_VERSION_SHA256SUM}" ]]; then
